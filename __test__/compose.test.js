c4c0d303ddfc76f8f7768e6cf12de291e5cb84c5
chenzesam <chenzesam@gmail.com>
2018-11-24 11:15:57 +0800

    @feature 添加 compose @desc 模仿 redux 的 compose

---
+++ __test__/compose.test.js
@@ -0,0 +1,20 @@
+import { compose, composeReverse } from '../src/compose'
+
+describe('compose', function () {
+
+  it('compose', () => {
+    expect(compose([
+      a => a + 3,
+      a => a + 2,
+      a => a + 1
+    ])(0)).toBe(6)
+  })
+
+  it('composeReverse', () => {
+    expect(compose([
+      a => a + 3,
+      a => a + 2,
+      a => a + 1
+    ])(0)).toBe(6)
+  })
+})
---
+++ src/compose.js
@@ -0,0 +1,14 @@
+import reduce from './reduce'
+
+const compose = (atoms) => {
+  return atoms.reduce((prev, next) => (...args) => prev(next(...args)))
+}
+
+const composeReverse = (atoms) => {
+  return atoms.reduceRight((prev, next) => (...args) => prev(next(...args)))
+}
+
+export {
+  compose,
+  composeReverse
+}

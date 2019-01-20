e7c219c31d5f29d043f75e9162c081a73529be76
chenzesam <chenzesam@gmail.com>
2019-01-20 16:48:44 +0800

    @feature Object.assign @desc 添加 Object.assign 的模仿

---
+++ __test__/assign.test.js
@@ -0,0 +1,25 @@
+import '../src/assign'
+
+describe('assign', function () {
+  it('common', () => {
+    let a = {}
+    let b = { name: 'test', age: 19 }
+    expect(Object.assignPolyfill(a, b)).toEqual({
+      name: 'test',
+      age: 19
+    })
+
+    expect(Object.assignPolyfill(a, b)).toBe(a)
+  })
+
+  it('complex test', () => {
+    let a = {}
+    let b = { name: 'test-b', age: 20, children: [{ name: 'test-b-children' }] }
+    let c = { name: 'test-c' }
+    expect(Object.assignPolyfill(a, b, c)).toEqual({
+      name: 'test-c',
+      age: 20,
+      children: [{ name: 'test-b-children' }]
+    })
+  })
+})
---
+++ src/assign.js
@@ -0,0 +1,15 @@
+Object.assignPolyfill = function (target, ...sources) {
+  if (target === null && target === undefined) {
+    throw Error('target must be variable')
+  }
+  let to = Object(target)
+  sources.forEach(source => {
+    for (const key in source) {
+      if (source.hasOwnProperty(key)) {
+        const value = source[key]
+        to[key] = value
+      }
+    }
+  })
+  return to
+}

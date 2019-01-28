74866d2e870bc3bc31a34ac1856001524f6c31ed
chenzesam <chenzesam@gmail.com>
2019-01-28 13:10:17 +0800

    @docs 补充注释

--- src/bind.js
+++ src/bind.js
@@ -10,11 +10,11 @@ Function.prototype.bindPolyfill = function (ctx, ...rest) {
   function Obj () {}
 
   function Bound (...innerRest) {
-    // 如果使用了 new Fn.bind(ctx) 那么 this 的原型是 Obj, 这时候的上下文应该交回去, 否则使用传入的上下文
-    return _this.apply(this instanceof Obj ? this : ctx, [...rest, ...innerRest])
+    // 如果使用了 new Fn.bind(ctx) 那么 this 的原型是 Bound, 这时候的上下文应该交回去, 否则使用传入的上下文
+    return _this.apply(this instanceof Bound ? this : ctx, [...rest, ...innerRest])
   }
 
-  // 原型链转移
+  // 原型链转移, 否则修改 instance 的 prototype 会修改到 this 的 prototype
   Obj.prototype = this.prototype
   Bound.prototype = new Obj()
 

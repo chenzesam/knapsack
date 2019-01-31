da853b2430d39f101bda81a616a175ec02900765
chenzesam <chenzesam@gmail.com>
2019-01-31 19:03:45 +0800

    @feature 添加算法 @desc 击鼓传花(队列) / 十进制转其他进制(栈)

---
+++ src/algorithms/divideByDecimal.js
@@ -0,0 +1,28 @@
+import Stack from '../data-structures/Stack'
+
+/**
+ * @description 十进制转其他进制
+ * @param {*} num 十进制数
+ * @param {*} decimal 其他进制
+ * @returns {string} 转化后的值
+ */
+function divideByDecimal (num, decimal) {
+  const stack = new Stack()
+  const digits = '0123456789ABCDEF'
+  let rem = null
+  let result = ''
+
+  while (num > 0) {
+    // 余数, 即二进制数
+    rem = Math.floor(num % decimal)
+    // 剩下的值
+    num = Math.floor(num / decimal)
+    stack.push(rem)
+  }
+  while (!stack.isEmpty()) {
+    result += digits[stack.pop()]
+  }
+  return result
+}
+
+export default divideByDecimal
---
+++ src/algorithms/hotPotato.js
@@ -0,0 +1,19 @@
+import Queue from '../data-structures/Queue'
+
+function hotPotato (nameList, num) {
+  const queue = new Queue()
+  nameList.forEach(name => {
+    queue.enqueue(name)
+  })
+  while (queue.size() > 1) {
+    let eliminated = null
+    for (let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue)
+    }
+    eliminated = queue.dequeue()
+    console.log(eliminated + '被淘汰')
+  }
+  return queue.dequeue()
+}
+
+export default hotPotato

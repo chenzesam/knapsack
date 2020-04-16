3fdfca3821eda70f136e0b086affb0ae383f3630
chenzesam <chenzesam@gmail.com>
2020-04-16 13:32:07 +0800

    优化

--- src/algorithms/swap-nodes-in-pairs.js
+++ src/algorithms/swap-nodes-in-pairs.js
@@ -1,16 +1,18 @@
-import { LinkedNode } from '../data-structures/linked-list/linked-list'
-
 function swapNodesInPairs (head) {
-  let parent = new LinkedNode(null)
-  parent.next = head
-  let prev = parent
+  if (!head) return null
+  let prev = { next: head }
+
+  // 获取当前的第二个节点, 用于返回结果
+  let resultHead = prev.next.next
+  if (!resultHead) return head
+
   while (prev.next && prev.next.next) {
     let first = prev.next
     let second = prev.next.next;
     [prev.next, second.next, first.next] = [second, first, second.next]
     prev = first
   }
-  return parent.next
+  return resultHead
 }
 
 export default swapNodesInPairs

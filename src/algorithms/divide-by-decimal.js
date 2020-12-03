82f80435c2a504538f51735d17591017f04a446d
chenzesam <chenzesam@gmail.com>
2020-12-03 17:56:46 +0800

    feat：回过头来看看以前的傻逼写法。。。

--- src/algorithms/divide-by-decimal.js
+++ src/algorithms/divide-by-decimal.js
@@ -1,28 +1,22 @@
-import Stack from '../data-structures/stack'
-
 /**
  * @description 十进制转其他进制
- * @param {*} num 十进制数
- * @param {*} decimal 其他进制
+ * @param number} num 十进制数
+ * @param {number} format 其他进制
  * @returns {string} 转化后的值
  */
-function divideByDecimal (num, decimal) {
-  const stack = new Stack()
-  const digits = '0123456789ABCDEF'
-  let rem = null
-  let result = ''
+const divideByDecimal = (num, format) => {
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  if (format < 2 || format > 36) {
+    return false;
+  }
+  while (num) {
+    const rem = digits[num % format];
+    num = Math.floor(num / format);
+    result = rem + result;
+  }
 
-  while (num > 0) {
-    // 余数, 即二进制数
-    rem = Math.floor(num % decimal)
-    // 剩下的值
-    num = Math.floor(num / decimal)
-    stack.push(rem)
-  }
-  while (!stack.isEmpty()) {
-    result += digits[stack.pop()]
-  }
-  return result
+  return result;
 }
 
 export default divideByDecimal

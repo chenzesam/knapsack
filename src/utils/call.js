68f4445182e38462b4a6fa77b0e7fd0990853280
chenzesam <chenzesam@gmail.com>
2020-12-05 16:45:39 +0800

    feature：简约写法。

--- src/utils/call.js
+++ src/utils/call.js
@@ -1,14 +1,10 @@
 /* eslint-disable */
-Function.prototype.callPolyfill = function (ctx) {
-  // 如果 ctx 传入的是 null
-  const context = ctx || window
-
+Function.prototype.callPolyfill = function (ctx = window, ...args) {
+  
   const fn = Symbol('fn')
-
   // 给上下文赋值新的调用函数
   context[fn] = this
-
-  const args = [...arguments].slice(1)
+  
   const result = context[fn](...args)
 
   delete context[fn]

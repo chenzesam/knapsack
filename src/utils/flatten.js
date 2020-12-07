e7296c24e14a22f102618c5c317ce56c9fdec796
chenzesam <chenzesam@gmail.com>
2020-12-07 12:54:29 +0800

    feat：扁平化数组添加层级。

--- src/utils/flatten.js
+++ src/utils/flatten.js
@@ -3,8 +3,11 @@
  * @param {[]} arr
  * @returns {[]}
  */
-function flatten (arr) {
+function flatten (arr, level = Infinity) {
+  if (level <= 0) return arr
   while (arr.some(v => Array.isArray(v))) {
+    if (level <= 0) return arr;
+    level--;
     arr = [].concat(...arr)
   }
   return arr
@@ -15,16 +18,29 @@ function flatten (arr) {
  * @param {[]} arr
  * @returns {[]}
  */
-function recursiveFlatten (arr) {
+function recursiveFlatten (arr, level = Infinity) {
+  if (level <= 0) return arr
   let result = []
   arr.forEach(v => {
     if (Array.isArray(v)) {
-      result = result.concat(recursiveFlatten(v))
+      result = result.concat(recursiveFlatten(v, level - 1))
     } else {
       result.push(v)
     }
   })
   return result
+}
+
+/**
+ * @description reduce 版扁平化数组
+ * @param {[]} arr
+ * @returns {[]}
+ */
+function flat(arr, level = Infinity) {
+  if (level <= 0) return arr
+  return arr.reduce(function (prev, cur) {
+    return prev.concat((Array.isArray(cur) && needFlat) ? flat(cur, level - 1) : cur)
+  }, [])
 }
 
 export { flatten, recursiveFlatten }

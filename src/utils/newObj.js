50e07efecc7ae07e46007e33ac09e4a3a3c356ac
陈森森 <chenzesam@gmail.com>
2021-02-07 21:25:15 +0800

    Update newObj.js

--- src/utils/newObj.js
+++ src/utils/newObj.js
@@ -11,10 +11,10 @@ function newObj (Obj, ...rest) {
   const res = Obj.apply(obj, rest)
 
   // 这里的 res 会根据 Obj 构造函数的返回值而定
-  // Obj 一般有两种情况: 1. 返回基础值 2. 返回其他对象（通过判断 typeof）
-  // 情况 1 下, 则相当于正常的 new 返回 this
-  // 情况 2 下, 则返回一个新的 object
-  // 所以需要判断一下构造函数是否返回了 object（情况二）从而进行 return
+  // Obj 一般有两种情况: 1. 返回其他对象（通过判断 typeof） 2. 返回基础值。
+  // 情况 1 下, 则返回一个新的 object。
+  // 情况 2 下, 则相当于正常的 new 返回 this。
+  // 所以需要判断一下构造函数是否返回了 object（情况 1）从而进行 return。
   return typeof res === 'object' ? res : obj
 }
 

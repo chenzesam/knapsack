b30502189471091dd0fe5ae8fa630c36d35f01e6
chenzesam <chenzesam@gmail.com>
2021-02-21 11:13:16 +0800

    feat：修改注释

--- src/utils/newObj.js
+++ src/utils/newObj.js
@@ -1,7 +1,6 @@
 /**
  * @description new 的过程
  * @param {object} Obj 对象
- * @param {any:[]} rest 参数
  * @returns {object}
  */
 function newObj (Obj, ...rest) {
@@ -11,7 +10,7 @@ function newObj (Obj, ...rest) {
   var res = Obj.apply(obj, rest)
 
   // 这里的 res 会根据 Obj 构造函数的返回值而定
-  // Obj 一般有三种情况: 1. 返回基础值 2. 返回对象
+  // Obj 一般有两种情况: 1. 返回基础值 2. 返回对象
   // 情况 1 下, 则相当于返回 this
   // 情况 2 下, 则返回一个新的 object
   // 所以需要判断一下构造函数是否返回了 object(情况二) 从而进行 return

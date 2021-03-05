9787b69ebc6e8c0377cef829647ddc2500c8011f
chenzesam <chenzesam@gmail.com>
2021-03-05 13:33:37 +0800

    fix：首字母不大写

--- src/utils/toCamel.js
+++ src/utils/toCamel.js
@@ -1,5 +1,5 @@
 function toCamel (str) {
-  return str.replace(/(^\w)|_(\w)/g, (m, $1, $2) => $1 ? $1.toUpperCase() : $2.toUpperCase())
+  return str.replace(/(^\w)|_(\w)/g, (m, $1, $2) => $1 ? $1 : $2.toUpperCase())
 }
 
 export default toCamel
